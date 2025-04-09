7f52bc55f628a9531f27d92ed7a311625c22a0b0
gonzalos20 <154982637+gonzalos20@users.noreply.github.com>
2025-04-09 16:14:59 -0300

    Changes on SW

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'note-app-cache-v1';
-const urlsToCache = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json',   '/icons/icon-192.png',
+const urlsToCache = ['./', './index.html', './styles.css', './app.js', './manifest.json',   './icons/icon-192.png',
   '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
