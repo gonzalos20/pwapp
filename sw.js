6ad88e4d194e8855a1ee54189a340931567f2af1
gonzalos20 <154982637+gonzalos20@users.noreply.github.com>
2025-04-09 16:46:33 -0300

    improvments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'note-app-cache-v3';
+const CACHE_NAME = 'note-app-cache-v1';
 const urlsToCache = ['./', './index.html', './styles.css', './app.js', './manifest.json',   './icons/icon-192.png',
   '/icons/icon-512.png'];
 
