f53dce8b71348f33d8df6a963dc7f45ccbfd4741
gonzalos20 <154982637+gonzalos20@users.noreply.github.com>
2025-04-09 16:19:21 -0300

    Changes on SW

--- sw.js
+++ sw.js
@@ -2,13 +2,15 @@ const CACHE_NAME = 'note-app-cache-v1';
 const urlsToCache = ['./', './index.html', './styles.css', './app.js', './manifest.json',   './icons/icon-192.png',
   '/icons/icon-512.png'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(urlsToCache);
+      }).catch((err) => {
+        console.error("Cache addAll failed:", err);
+      })
+    );
+  });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
